c25ec88ffb60c52b78089c7ff31db81bab703be8
Jamison Coombs <58711684+JCoombs224@users.noreply.github.com>
2023-03-26 13:25:24 -0700

    Added firebase for backend and hosting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home/home.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -17,6 +25,13 @@ import { HomeComponent } from './home/home/home.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    provideStorage(() => getStorage()),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'fithub-94c8b',
+    appId: '1:244375237867:web:11919289c4dfaf33643aae',
+    databaseURL: 'https://fithub-94c8b-default-rtdb.firebaseio.com',
+    storageBucket: 'fithub-94c8b.appspot.com',
+    apiKey: 'AIzaSyCvmy6J38YSX20HvRSDXVB2uuQQEsMXiS8',
+    authDomain: 'fithub-94c8b.firebaseapp.com',
+    messagingSenderId: '244375237867',
+    measurementId: 'G-ZXKHGE568L',
+  },};
