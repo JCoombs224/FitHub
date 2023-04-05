66a1b58b94461519d5cebd616001aafa29f768d1
Jamison Coombs <jamiedude224@gmail.com>
2023-04-05 15:21:44 -0700

    added link

--- src/app/pages/my-workouts/my-workouts.component.ts
+++ src/app/pages/my-workouts/my-workouts.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { AuthService } from 'src/app/services/auth.service'; 
+import { ToastrService } from 'ngx-toastr';
+import { CurrentUserService } from 'src/app/services/current-user.service';
 import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -8,4 +15,19 @@ import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 })
 export class MyWorkoutsComponent {
   faPlusCircle = faPlusCircle;
+
+  constructor(private router: Router,
+    private title: Title,
+    private fb: FormBuilder,
+    public authService: AuthService,
+    private toastr: ToastrService,
+    public currentUser: CurrentUserService) {}
+
+  ngOnInit(): void {
+    this.title.setTitle("My Workouts | FitHub");
+  }
+
+  newWorkout() {
+    this.router.navigate(["my-workouts/create"]);
+  }
 }
