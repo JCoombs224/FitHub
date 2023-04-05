6845d22f7f44f7f8a6f1e23417812c68d641dd5f
Jamison Coombs <58711684+JCoombs224@users.noreply.github.com>
2023-04-05 11:31:19 -0700

    Fix: Added current user service to my profile component

--- src/app/pages/my-profile/my-profile.component.ts
+++ src/app/pages/my-profile/my-profile.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { AuthService } from 'src/app/services/auth.service'; 
+import { ToastrService } from 'ngx-toastr';
+import { CurrentUserService } from 'src/app/services/current-user.service';
 
 @Component({
   selector: 'app-my-profile',
@@ -6,5 +13,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-profile.component.css']
 })
 export class MyProfileComponent {
+  
+  
+  constructor(private router: Router,
+              private title: Title,
+              private fb: FormBuilder,
+              public authService: AuthService,
+              private toastr: ToastrService,
+              public currentUser: CurrentUserService) {}
 
+  ngOnInit(): void {
+    this.title.setTitle("My Profile | FitHub");
+  } 
+  
 }
