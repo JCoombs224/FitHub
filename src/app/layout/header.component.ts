ab8e828d8040bcce657995aeb101d650389a4a9a
Jamison Coombs <58711684+JCoombs224@users.noreply.github.com>
2023-05-03 20:27:55 -0700

    finished search bar

--- src/app/layout/header.component.ts
+++ src/app/layout/header.component.ts
@@ -2,7 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { CurrentUserService } from '../services/current-user.service';
 import { Router } from '@angular/router';
-import { faHouse, faDumbbell, faFeed, faUser, faCog } from '@fortawesome/free-solid-svg-icons';
+import { faHouse, faDumbbell, faFeed, faUser, faCog, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { Observable } from 'rxjs';
+import { ProfileService } from '../services/profile.service';
 
 @Component({
   selector: 'app-header-section',
@@ -15,6 +17,14 @@ export class HeaderComponent implements OnInit {
   faFeed = faFeed;
   faUser = faUser;
   faCog = faCog;
+  faSearch = faSearch;
+
+  // used for search bar requests
+  private debounceTimer = null;
+
+  searchQuery = '';
+  searchResults;
+  subscription;
 
   user = this.currentUserService.user;
   dropdownOpen = false;
@@ -22,6 +32,7 @@ export class HeaderComponent implements OnInit {
 
   constructor(public authService: AuthService,
               private currentUserService: CurrentUserService,
+              private profileService: ProfileService,
               public router: Router) {}
 
   ngOnInit() {
@@ -29,10 +40,41 @@ export class HeaderComponent implements OnInit {
       this.mobile = true;
     }
   }
+
   reload(){
     window.location.reload();
   }
+
   logOut(){
     this.authService.SignOut();
   }
+
+  search() {
+    if(this.searchQuery.length < 1) {
+      this.searchResults = null;
+      return;
+    }
+
+    if(this.debounceTimer) {
+      clearTimeout(this.debounceTimer);
+    }
+
+    if (this.searchQuery != '' && this.searchQuery.trim() != '') {
+      this.debounceTimer = setTimeout(() => {
+        this.subscription = this.profileService.searchProfiles(this.searchQuery).subscribe(results => {
+          this.searchResults = results;
+          this.subscription.unsubscribe();
+        });
+      }, 500);
+    } else {
+      this.searchResults = null;
+    }
+  }
+
+  clearSearch() {
+    this.searchQuery = '';
+    this.searchResults = null;
+    this.subscription.unsubscribe();
+  }
+
 }
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+import { Observable, debounceTime } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +8,8 @@ import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat
 export class ProfileService {
 
   constructor(public afs: AngularFirestore) { }
+
+  profilesCollection: any;
 
   get initProfile() {
     return {
@@ -31,5 +34,16 @@ export class ProfileService {
 
   getProfile(username): AngularFirestoreDocument<any> {
     return this.afs.doc(`/profiles/${username}`);
+  }
+
+  searchProfiles(query: string): Observable<any> {
+    this.profilesCollection = this.afs.collection<any>('profiles', ref =>
+      ref.where('profileHandle', '>=', query)
+         .where('profileHandle', '<=', query + '\uf8ff')
+         .orderBy('profileHandle')
+         .limit(10)
+    );
+    
+    return this.profilesCollection.valueChanges();
   }
 }
